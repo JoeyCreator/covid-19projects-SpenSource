b32e892a341604ec3a74e13f0bd210584b66ce01
Joey Cadieux <58401407+JoeyCreator@users.noreply.github.com>
2020-12-04 19:55:05 -0500

    Dev open source

---
+++ system-config/dev-console/console.js
@@ -0,0 +1,14 @@
+console.log(
+  "%cStop!",
+  "color:red;font-family:system-ui;font-size:4rem;-webkit-text-stroke: 1px black;font-weight:bold"
+);
+
+console.log(
+  "%cThis is a browser feature intended for developers.",
+  "color:black;font-family:system-ui;font-size:4rem;-webkit-text-stroke: 1px black;font-weight:arial"
+);
+
+console.log(
+  "%cSee https://softwareqc.netlify.app for more information.",
+  "color:black;font-family:system-ui;font-size:1.2rem;-webkit-text-stroke: 1px black;font-weight:arial"
+);
